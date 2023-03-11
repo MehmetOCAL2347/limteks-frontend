4c7935622b4f5c8cea514ad1667a13d3acd03eb2
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-11 14:44:47 +0300

    app.vue style edited

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,33 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+  min-height: 100%;
+}
+
+body {
+  margin: 0;
+}
 #app-body{
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 }
+
+li {
+  list-style-type: none;
+}
+a {
+  text-decoration: none;
+}
+
+/* :root { // Renkleri gircez
+} */
 
 </style>
