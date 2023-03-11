0c8c9ffbeb8f5f31495506e90274f510bc53ae4b
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-11 14:16:02 +0300

    router added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue'
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        
-    ]
-})
+import App from './App.vue';
+import router from './router.js';
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
